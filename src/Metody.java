cf39f6968edc9df635ab6837927cf6a96cbe98c5
JustynaQA <justyna.anna.porebska@gmail.com>
2021-12-08 22:55:00 +0100

    Commit

--- src/Metody.java
+++ src/Metody.java
@@ -1,15 +1,44 @@
 public class Metody {
 
-    public void policzWynik(){
-        //definiujemy instrukcje dla metody
-        System.out.println("Oblicz wynik");
-        int result = 0;
-        for (int i = 0; i<100 ; i++){
-            result = result + i;
+    public void policzWynik() {
 
+        //definiujemy instrukcje dla metody, która NIC NIE ZWRACA
+        System.out.println("zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            result = result + i;
         }
         System.out.println(result);
     }
 
+    //definiujemy metodę, która coś zwraca > trzeba napisać RETURN!
+    public int pobierzWynik() {
+        System.out.println("zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            result = result + i;
+        }
+        System.out.println(result);
+        return result;
+    }
 
+    public void policzWynikElastyczna(int number) {
+        System.out.println("Number: " + number);
+        System.out.println("zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result + i;
+        }
+        System.out.println(result);
+    }
+
+    public int add(int firstNumber, int secondNumber, String word) {
+        System.out.println("suma to: " + (firstNumber + secondNumber));
+//        if(firstNumber==0) {
+//        return 0;
+//        }
+        return (firstNumber+secondNumber);
+
+    }
 }
+
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -1,11 +1,17 @@
 public class MetodyTest {
-
     public static void main(String[] args) {
         Metody metody = new Metody();
+//        metody.policzWynik();
+//        int wynik = metody.pobierzWynik();
+//        System.out.println(wynik);
+//        int wynik2 = wynik*2;
+//        System.out.println("wynik po mnożeniu to: " + wynik2);
 
-        metody.policzWynik();
-        metody.policzWynik();
-        metody.policzWynik();
-        metody.policzWynik();
+        metody.policzWynikElastyczna(5);
+        metody.policzWynikElastyczna(99);
+
+        metody.add(2, 3, "Hello");
+        int result = metody.add(2, 3, "Hello");
+
     }
 }
